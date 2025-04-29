0aa71b6b4f3d64317b8229921d0d90ac26674d63
bainur-5 <bainur05ab@gmail.com>
2025-04-29 22:32:14 +0600

    learing controls

--- src/script.js
+++ src/script.js
@@ -1,80 +1,55 @@
-import * as THREE from 'three';
-
-import './style.css';
+import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import "./style.css";
 
 // Сцена
 const scene = new THREE.Scene();
-const canvas = document.querySelector('.canvas');
+const canvas = document.querySelector(".canvas");
 
 // Камера
 const sizes = {
-    width: 600,
-    height: 600,
+  width: 600,
+  height: 600,
 };
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
 
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 scene.add(camera);
 
 // Объект
 const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+  color: "yellow",
+  wireframe: true,
+});
 
+const mesh = new THREE.Mesh(geometry, material);
 
-const group = new THREE.Group();
-const meshes = [];
+scene.add(mesh);
 
-const colors = [0xb78d8, 0xe86344, 0xe8ab9c];
-for (let x = -1.2; x <= 1.2; x = x + 1.2) {
-    for (let y = -1.2; y <= 1.2; y = y + 1.2) {
-        const material = new THREE.MeshBasicMaterial({
-            color: colors[((Math.random() * 3) | 0)],
-            wireframe: true,
-        });
-        const mesh = new THREE.Mesh(geometry, material);
-        mesh.scale.set(0.5, 0.5, 0.5)
-        mesh.position.set(x, y, 0);
-        meshes.push(mesh);
-    }
-}
-
-group.add(...meshes);
-scene.add(group);
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
-const Max_Scale = 1;
-const Min_Scale = .5;
-let grow = false;
+const cursor = {
+  x: 0,
+  y: 0,
+};
+window.addEventListener("mousemove", (e) => {
+  cursor.x = -(e.clientX / sizes.width - 0.5);
+  cursor.y = e.clientY / sizes.height - 0.5;
+});
 
-const clock = new THREE.Clock()
-const animate = () =>{
-    const delta = clock.getDelta();
-    meshes.forEach((item, i) => {
-        const mult = i % 2 === 0 ? 1 : -1;
-        
-        item.rotation.x += mult * delta
-        item.rotation.y += mult * delta * .4
-    })
-
-    const elepsedTime = clock.getElapsedTime();
-    camera.position.x = Math.sin(elepsedTime)
-    camera.position.y = Math.cos(elepsedTime)
-    camera.lookAt(new THREE.Vector3(0, 0, 0))
-
-    const mult = grow ? 1 : -1;
-    group.scale.x += mult * delta * .5
-    group.scale.y += mult * delta * .5
-    group.scale.z += mult * delta * .5
-
-    if (grow && group.scale.x >= Max_Scale) {
-        grow = false
-    }else if(group.scale.x <= Min_Scale){
-        grow = true
-    }
-
-    renderer.render(scene, camera)
-    // window.requestAnimationFrame(animate)
-}
-animate()
+const tick = () => {
+  //   camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2;
+  //   camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2;
+  //   camera.position.y = cursor.y;
+  //   camera.lookAt(mesh.position);
+  controls.update();
+  renderer.render(scene, camera);
+  requestAnimationFrame(tick)
+};
+tick();
