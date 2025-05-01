efd12f1461b27869840df9b2b810fbd5f0757bb2
bainur-5 <bainur05ab@gmail.com>
2025-05-01 14:58:19 +0600

    learing more three geometry

--- src/script.js
+++ src/script.js
@@ -1,15 +1,19 @@
-import * as THREE from "three";
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
-import "./style.css";
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
-// Сцена
+import './style.css';
+
 const scene = new THREE.Scene();
-const canvas = document.querySelector(".canvas");
+const canvas = document.querySelector('.canvas');
 
-// Камера
 const sizes = {
-  width: 600,
-  height: 600,
+    width: window.innerWidth,
+    height: window.innerHeight,
+};
+
+const cursor = {
+    x: 0,
+    y: 0,
 };
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
@@ -17,13 +21,26 @@ camera.position.z = 3;
 
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
+
 scene.add(camera);
 
 // Объект
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const geometry = new THREE.CircleGeometry(1, 20, 0, Math.PI)
+// const geometry = new THREE.PlaneGeometry(1, 1, 20, 20)
+// const geometry = new THREE.ConeGeometry(1, 2, 32, 1, false, 0, Math.PI )
+// const geometry = new THREE.CylinderGeometry(0.5, 1, 2, 32, 1, true, 0)
+// const geometry = new THREE.RingGeometry(0.5, 1, 32, 10, 0, Math.PI * 2 )
+// const geometry = new THREE.TorusGeometry(1, 0.5, 16, 60)
+// const geometry = new THREE.TorusKnotGeometry(1, .4, 100, 10, 3, 4)
+// const geometry = new THREE.DodecahedronGeometry(1, 0)
+// const geometry = new THREE.OctahedronGeometry(1, 0)
+// const geometry = new THREE.TetrahedronGeometry(1, 0)
+// const geometry = new THREE.IcosahedronGeometry(1, 0)
+// const geometry = new THREE.SphereGeometry(1, 32, 16, 0, Math.PI * 2, 0, Math.PI / 2)
 const material = new THREE.MeshBasicMaterial({
-  color: "yellow",
-  wireframe: true,
+    color: 'yellow',
+    wireframe: true,
 });
 
 const mesh = new THREE.Mesh(geometry, material);
@@ -34,22 +51,34 @@ const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
 
-const cursor = {
-  x: 0,
-  y: 0,
+const tick = () => {
+    controls.update();
+    renderer.render(scene, camera);
+
+    window.requestAnimationFrame(tick);
 };
-window.addEventListener("mousemove", (e) => {
-  cursor.x = -(e.clientX / sizes.width - 0.5);
-  cursor.y = e.clientY / sizes.height - 0.5;
+
+tick();
+
+window.addEventListener('resize', () => {
+    // Обновляем размеры
+    sizes.width = window.innerWidth;
+    sizes.height = window.innerHeight;
+
+    // Обновляем соотношение сторон камеры
+    camera.aspect = sizes.width / sizes.height;
+    camera.updateProjectionMatrix();
+
+    // Обновляем renderer
+    renderer.setSize(sizes.width, sizes.height);
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+    renderer.render(scene, camera);
 });
 
-const tick = () => {
-  //   camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 2;
-  //   camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 2;
-  //   camera.position.y = cursor.y;
-  //   camera.lookAt(mesh.position);
-  controls.update();
-  renderer.render(scene, camera);
-  requestAnimationFrame(tick)
-};
-tick();
+window.addEventListener('dblclick', () => {
+    if (!document.fullscreenElement) {
+        canvas.requestFullscreen();
+    } else {
+        document.exitFullscreen();
+    }
+});
